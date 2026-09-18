91391cb761895aeec6b6c9c43b0eff0b6007f0d7
molsousa <sousas1569@gmail.com>
2026-09-18 09:20:12 -0300

    adicionado script para exportar tabela de gantt

---
+++ python/ganttmpp.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../gantt_mpp.csv")
+df = df[df["pid"] != "-"]  # remove CPU ociosa
+
+fig, ax = plt.subplots(figsize=(14, 6))
+processos = df["pid"].unique()
+ypos = {p: i for i, p in enumerate(processos)}
+
+for _, row in df.iterrows():
+    ax.barh(ypos[row["pid"]], row["duracao"], left=row["tempo_inicio"],
+            color="steelblue", edgecolor="black")
+
+ax.set_yticks(list(ypos.values()))
+ax.set_yticklabels(list(ypos.keys()))
+ax.set_xlabel("Tempo")
+ax.set_title("Gantt - MPP")
+plt.tight_layout()
+plt.savefig("gantt_mpp.png", dpi=150)
